3b49e17ad2a2e311302e0492b1a1d25ac837af8c
jammy438 <lollidoll97@gmail.com>
2026-03-18 13:08:06 +0000

    feat: update mock data and add new data hooks

--- apps/web/lib/hooks/useSpendOverTime.ts
+++ apps/web/lib/hooks/useSpendOverTime.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { useQuery } from '@tanstack/react-query'
 import { useOrganization } from '@clerk/nextjs'
 import { mockSpendOverTimeResponse, SpendOverTimeResponse } from '../mockData'
